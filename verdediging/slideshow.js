035cf5006f6870600f9e46c74b2ac6a1ca494cc2
Tim Besard <tim.besard@gmail.com>
2011-09-10 12:08:43 +0200

    Upstream pull.

--- verdediging/slideshow.js
+++ verdediging/slideshow.js
@@ -1,6 +1,7 @@
 (function() {
 	var doc = document;
 	var disableBuilds = false;
+	var disableNotes = false;
 
 	var ctr = 0;
 	var spaces = /\s+/, a1 = [''];
@@ -128,6 +129,8 @@
 	//
 	var Slide = function(node, idx) {
 		this._node = node;
+		var note = query('.note > section', node);
+		this._speakerNote = note ? note.innerHTML : '';
 		if (idx >= 0) {
 			this._count = idx + 1;
 		}
@@ -145,8 +148,8 @@
 		_visited: false,
 		_currentState: '',
 		_states: [ 'distant-slide', 'far-past',
-			'past', 'current', 'future',
-			'far-future', 'distant-slide' ],
+				 'past', 'current', 'future',
+				 'far-future', 'distant-slide' ],
 		setState: function(state) {
 			if (typeof state != 'string') {
 				state = this._states[state];
@@ -161,8 +164,8 @@
 
 			// delay first auto run. Really wish this were in CSS.
 			/*
-	this._runAutos();
-			 */
+			this._runAutos();
+			*/
 			var _t = this;
 			setTimeout(function(){ _t._runAutos(); } , 400);
 
@@ -215,7 +218,6 @@
 		_makeCounter: function() {
 			if(!this._count || !this._node) { return; }
 			var c = doc.createElement('span');
-			c.textContent = this._count;
 			c.className = 'counter';
 			this._node.appendChild(c);
 		},
@@ -244,12 +246,12 @@
 			});
 			if (idx >= 0) {
 				var elem = this._buildList.splice(idx, 1)[0];
- 
+
 				var _t = this;
 				if (canTransition) {
 					var l = function(evt) {
 						elem.parentNode.removeEventListener('webkitTransitionEnd', l, false);
-						elem.parentNode.removeEventListener('transitionend', l, false);  // moz
+						elem.parentNode.removeEventListener('transitionend', l, false);	// moz
 						elem.parentNode.removeEventListener('oTransitionEnd', l, false);
 						_t._runAutos();
 					};
@@ -264,6 +266,9 @@
 					}, 400);
 				}
 			}
+		},
+		getSpeakerNote: function() {
+			return this._speakerNote;
 		},
 		buildNext: function() {
 			if (!this._buildList.length) {
@@ -290,25 +295,34 @@
 			// if this happens is very likely that someone is coming from
 			// a link with the old permalink format, i.e. #slide24
 			alert('The format of the permalinks have recently changed. If you are coming ' +
-				'here from an old external link it\'s very likely you will land to the wrong slide');
+					 'here from an old external link it\'s very likely you will land to the wrong slide');
 			this.current = "landing-slide";
 		}
 		var _t = this;
 		doc.addEventListener('keydown',
-		function(e) { _t.handleKeys(e); }, false);
+				function(e) { _t.handleKeys(e); }, false);
 		doc.addEventListener('touchstart',
-		function(e) { _t.handleTouchStart(e); }, false);
+				function(e) { _t.handleTouchStart(e); }, false);
 		doc.addEventListener('touchend',
-		function(e) { _t.handleTouchEnd(e); }, false);
+				function(e) { _t.handleTouchEnd(e); }, false);
 		window.addEventListener('popstate',
-		function(e) { if (e.state) { _t.go(e.state, true); } }, false);
+				function(e) { if (e.state) { _t.go(e.state, true); } }, false);
 		query('#left-init-key').addEventListener('click',
-		function() { _t.next(); }, false);
+				function() { _t.next(); }, false);
 		this._update();
+		queryAll('#nav-prev, #nav-next').forEach(function(el) {
+			el.addEventListener('click', _t.onNavClick.bind(_t), false);
+		});
+		queryAll('menu button').forEach(function(el) {
+			el.addEventListener('click', _t.onCommandClick.bind(_t), false);
+		});
 	};
 
 	SlideShow.prototype = {
 		_presentationCounter: query('#presentation-counter'),
+		_menuCounter: query('#slide-no'),
+		_speakerNote: query('#speaker-note'),
+		_help: query('#help'),
 		_slides: [],
 		_getCurrentIndex: function() {
 			var me = this;
@@ -325,30 +339,34 @@
 			// the slides are already loaded (so we show the loading... instead)
 			// the technique to test visibility is taken from here
 			// http://stackoverflow.com/questions/704758/how-to-check-if-an-element-is-really-visible-with-javascript
-            var currentIndex = this._getCurrentIndex();
+			var currentIndex = this._getCurrentIndex();
 
-            if (targetId) {
-                var savedIndex = currentIndex;
-                this.current = targetId;
-                currentIndex = this._getCurrentIndex();
-                if (Math.abs(savedIndex - currentIndex) > 1) {
-                    // if the current switch is not "prev" or "next", we need clear
-                    // the state setting near the original slide
-                    for (var x = savedIndex; x < savedIndex + 7; x++) {
-                        if (this._slides[x-4]) {
-                            this._slides[x-4].setState(0);
-                        }
-                    }
-                }
-            }
+			if (targetId) {
+				var savedIndex = currentIndex;
+				this.current = targetId;
+				currentIndex = this._getCurrentIndex();
+				if (Math.abs(savedIndex - currentIndex) > 1) {
+					// if the current switch is not "prev" or "next", we need clear
+					// the state setting near the original slide
+					for (var x = savedIndex; x < savedIndex + 7; x++) {
+						if (this._slides[x-4]) {
+							this._slides[x-4].setState(0);
+						}
+					}
+				}
+			}
 			var docElem = document.documentElement;
 			var elem = document.elementFromPoint( docElem.clientWidth / 2, docElem.clientHeight / 2);
 			if (elem && elem.className != 'presentation') {
 				this._presentationCounter.textContent = currentIndex;
+				if (this._menuCounter) {
+					this._menuCounter.textContent = currentIndex;					
+				}
 			}
+			this._speakerNote.innerHTML = this._slides[currentIndex - 1].getSpeakerNote();
 			if (history.pushState) {
 				if (!dontPush) {
-					history.replaceState(this.current, 'Slide ' + this.current, '#' + this.current);
+					history.pushState(this.current, 'Slide ' + this.current, '#' + this.current);
 				}
 			} else {
 				window.location.hash = this.current;
@@ -364,20 +382,27 @@
 		next: function() {
 			if (!this._slides[this._getCurrentIndex() - 1].buildNext()) {
 				var next = query('#' + this.current + ' + .slide');
-                //this.current = (next) ? next.id : this.current;
-                this._update((next) ? next.id : this.current);
+				//this.current = (next) ? next.id : this.current;
+				this._update((next) ? next.id : this.current);
 			}
 		},
 		prev: function() {
 			var prev = query('.slide:nth-child(' + (this._getCurrentIndex() - 1) + ')');
-            //this.current = (prev) ? prev.id : this.current;
-            this._update((prev) ? prev.id : this.current);
+			//this.current = (prev) ? prev.id : this.current;
+			this._update((prev) ? prev.id : this.current);
 		},
-        go: function(slideId, dontPush) {
-            //this.current = slideId;
-            this._update(slideId, dontPush);
+		go: function(slideId, dontPush) {
+			//this.current = slideId;
+			this._update(slideId, dontPush);
 		},
 
+		showNotes: function() {
+			if (disableNotes) {
+				return;
+			}
+			this._speakerNote.style.display = "block";
+			this._speakerNote.classList.toggle('invisible');
+		},
 		switch3D: function() {
 			toggleClass(document.body, 'three-d');
 		},
@@ -393,20 +418,32 @@
 			linkEls[(sheetIndex + 1) % linkEls.length].disabled = false;
 			sessionStorage['theme'] = linkEls[(sheetIndex + 1) % linkEls.length].href;
 		},
+		toggleHelp: function() {
+			this._help.style.display = "block";
+			this._help.classList.toggle('invisible');
+		},
+		viewSource: function() {
+			window.open("view-source:" + window.location.href);
+		},
 		handleKeys: function(e) {
 			if (/^(input|textarea)$/i.test(e.target.nodeName) || e.target.isContentEditable) {
 				return;
 			}
-
 			switch (e.keyCode) {
-				case 37: // left arrow
+				case 37:	// left arrow
 					this.prev(); break;
-				case 39: // right arrow
-				case 32: // space
+				case 39:	// right arrow
+				case 32:	// space
 					this.next(); break;
-				case 51: // 3
+				case 48:	// 0
+					this.toggleHelp(); break;
+				case 51:	// 3
 					this.switch3D(); break;
-				case 84: // T
+				case 78:	// N
+					this.showNotes(); break;
+				case 83:	// S
+					this.viewSource(); break;
+				case 84:	// T
 					this.changeTheme(); break;
 			}
 		},
@@ -423,33 +460,58 @@
 				this.prev();
 			}
 		},
+		onNavClick: function(e) {
+			if (e.target.id == "nav-prev") {
+				this.prev();
+			} else if (e.target.id = "nav-next") {
+				this.next();
+			}
+		},
+		onCommandClick: function(e) {
+			var n = e.target.getAttribute('data-command');
+			switch(n) {
+			case 'toc':
+				this._update("table-of-contents"); break;
+			case 'resources':
+				break;
+			case 'notes':
+				this.showNotes(); break;
+			case 'source':
+				this.viewSource(); break;
+			case 'help':
+				this.toggleHelp(); break;
+			default:
+				return;
+			}
+		}
 	};
 
 	// disable style theme stylesheets
 	var linkEls = queryAll('link.theme');
-	    var stylesheetPath = 'css/default.css';
+	var stylesheetPath = 'css/default.css';
 	linkEls.forEach(function(stylesheet) {
 		stylesheet.disabled = !(stylesheet.href.indexOf(stylesheetPath) != -1);
 	});
 
 	// Initialize
-    var li_array = [];
-    var transitionSlides = queryAll('.transitionSlide').forEach(function(el) {
-        li_array.push( ['<li><a data-hash="', el.id, '">',
-        query('h2', el).textContent, '</a></li>'].join('')
-        );
-    });
+	var li_array = [];
+	var transitionSlides = queryAll('.transitionSlide').forEach(function(el) {
+		li_array.push( ['<li><a data-hash="', el.id, '">',
+										query('h2', el).textContent, '</a></li>'].join('')
+								 );
+	});
 
-    query('#toc-list').innerHTML = li_array.join('');
-    
+	query('#toc-list').innerHTML = li_array.join('');
+
 	var slideshow = new SlideShow(queryAll('.slide'));
-
+	
 	document.addEventListener('DOMContentLoaded', function() {
 		query('.slides').style.display = 'block';
 	}, false);
-	
-    queryAll('#toc-list li a').forEach(function(el) {
-        el.onclick = function() { slideshow.go(el.dataset['hash']); };
-    });
 
+	queryAll('#toc-list li a').forEach(function(el) {
+			el.onclick = function() { slideshow.go(el.dataset['hash']); };
+	});	
 })();
+
+
